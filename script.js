cd9a7bac5a8c9385f6224e9fb09a4de503a5ef6a
ThunderKlappe <timschley@gmail.com>
2022-01-18 19:52:50 -0800

    CSS for modals working

---
+++ script.js
@@ -0,0 +1,19 @@
+//get starting elements
+const totalBooksLabel = document.querySelector("#total-books");
+const totalUnreadLabel = document.querySelector("#unread-books");
+const totalReadLabel = document.querySelector("#read-books");
+const averageRatingLabel = document.querySelector("#avg-rating");
+
+const addButton = document.querySelector("#add-book");
+const clearButton = document.querySelector("#clear-library");
+
+
+//event listeners to starting buttons
+addButton.addEventListener('click', newBookModal);
+clearButton.addEventListener('click', clearLibraryModal);
+
+
+//*******Functions*******
+function newBookModal(){
+    
+}
